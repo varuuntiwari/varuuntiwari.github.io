bba1df2dbff64389a3707d81b61db4249bb52194
Varun Tiwari <varuntiwari2001@gmail.com>
2021-09-06 23:32:34 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 var toggle=0;
 function toggleSide(){
     var value;
-    if(!toggle){value="250px"; toggle=1;}
-    else{value="0px"; toggle=0;}
+    if(!toggle){
+        value="250px";
+        toggle=1;
+    }
+    else{
+        value="0px";
+        toggle=0;
+    }
     document.getElementById("sidebar").style.width = value;
     document.getElementById("main").style.marginLeft = value;
 }
